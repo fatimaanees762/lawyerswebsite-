33fa6327263b6d40c51f052a01ab981da8ab2602
Maham <maham2006ahsan@gmail.com>
2025-09-03 12:58:05 +0500

    e-project

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\Lawyers;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+//Admin routes and middleware
+Route::middleware([Admin::class])->group(function(){
+
+});
+
+//Lawyers routes and middleware
+Route::middleware([Lawyers::class])->group(function(){
+
+});
